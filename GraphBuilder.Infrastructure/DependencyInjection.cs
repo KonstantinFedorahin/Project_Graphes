1ead0982ec96c65ed6f0f924ba80b77606e50609
Gall1ard <say12232@gmail.com>
2026-01-22 15:55:34 +0500

    Update DI container

--- GraphBuilder.Infrastructure/DependencyInjection.cs
+++ GraphBuilder.Infrastructure/DependencyInjection.cs
@@ -3,6 +3,8 @@ using GraphBuilder.Domain.Interfaces;
 using GraphBuilder.Infrastructure.Parsing;
 using GraphBuilder.Infrastructure.Calculating;
 using GraphBuilder.Domain.Services;
+using GraphBuilder.Infrastructure.GraphSerializers;
+using GraphBuilder.Infrastructure.GraphSerializer;
 
 namespace GraphBuilder.Infrastructure;
 
@@ -10,7 +12,10 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services)
     {
-        services.AddTransient<IBuildGraphService, BuildGraphService>();
+        services.AddSingleton<IGraphDataSerializer, CsvGraphSerializer>();
+        services.AddSingleton<IGraphDataSerializer, JsonGraphSerializer>();
+        services.AddSingleton<IGraphDataSerializer, TxtGraphSerializer>();
+        services.AddSingleton<IBuildGraphService, BuildGraphService>();
         services.AddSingleton<IFunctionParser, MathExpressionParser>();
         services.AddSingleton<ICalculatePoints, GraphCalculator>();
         return services;
